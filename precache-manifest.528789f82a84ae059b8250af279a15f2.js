888d1604af961cea467ae605be56ce403b965cc6
Daniel Laloush <daniel.laloush@influitive.com>
2021-10-17 12:28:50 -0400

    Updates

---
+++ precache-manifest.528789f82a84ae059b8250af279a15f2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88fcb069f229edde329240fb919b759e",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "44adb7745f1d7b525c67",
+    "url": "/Portafolio/static/css/main.ed52ac21.chunk.css"
+  },
+  {
+    "revision": "8a8c6ddd6f5ad5df36eb",
+    "url": "/Portafolio/static/js/2.176b348b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portafolio/static/js/2.176b348b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44adb7745f1d7b525c67",
+    "url": "/Portafolio/static/js/main.6b2c1c97.chunk.js"
+  },
+  {
+    "revision": "b602b082f5b144c4dc57",
+    "url": "/Portafolio/static/js/runtime-main.47324fb7.js"
+  },
+  {
+    "revision": "fbfa55e2da6cda46142b9b05e822d99c",
+    "url": "/Portafolio/static/media/Daniel Laloush Resumen.fbfa55e2.pdf"
+  },
+  {
+    "revision": "3d78bd82ac5c554dff998a487f200b56",
+    "url": "/Portafolio/static/media/Program.3d78bd82.gif"
+  },
+  {
+    "revision": "43e8ed6b89bf34d497e48a61b522b6e5",
+    "url": "/Portafolio/static/media/RoR.43e8ed6b.png"
+  },
+  {
+    "revision": "ab6a446763402ac2155db948e549d53a",
+    "url": "/Portafolio/static/media/api.ab6a4467.png"
+  },
+  {
+    "revision": "906cc4328259b74a7bc3e1bfea9afe23",
+    "url": "/Portafolio/static/media/embassy.906cc432.jpg"
+  },
+  {
+    "revision": "e93e7cc4fcab24936f6b978ee109f38e",
+    "url": "/Portafolio/static/media/game-play.e93e7cc4.gif"
+  },
+  {
+    "revision": "2d64dbab368fc75c8ad4200864d2c1e2",
+    "url": "/Portafolio/static/media/icon_x.2d64dbab.svg"
+  },
+  {
+    "revision": "1dc293298184909ae35c7caa76ad9fa3",
+    "url": "/Portafolio/static/media/logo.1dc29329.svg"
+  },
+  {
+    "revision": "f069bbe22ecfefad6f5d6b5a4393b76b",
+    "url": "/Portafolio/static/media/mobile.f069bbe2.png"
+  },
+  {
+    "revision": "27896590715de81539615daad128b986",
+    "url": "/Portafolio/static/media/ndm.27896590.png"
+  },
+  {
+    "revision": "27d6dda3b4a3ff2b3acdbb24b942ae1f",
+    "url": "/Portafolio/static/media/project-1.27d6dda3.png"
+  },
+  {
+    "revision": "2ccfe661ec02e84562cece9efa858514",
+    "url": "/Portafolio/static/media/stock.2ccfe661.png"
+  },
+  {
+    "revision": "a1223de504180a12574a17a1f30c5a08",
+    "url": "/Portafolio/static/media/twitter-square-brands.a1223de5.svg"
+  }
+]);
